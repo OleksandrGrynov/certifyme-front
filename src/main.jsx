11694e929085625f06d23e1199443072ce34fb66
OleksandrGrinov <alex170707228@gmail.com>
2025-10-27 23:14:28 +0200

    chore(core): initialize React app with i18n, Google OAuth and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./i18n/i18n";
 import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId =
+    "382994921409-a7plkvr0d3pns8e6uac4fn7go39lielq.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <GoogleOAuthProvider clientId={clientId}>
+            <App />
+        </GoogleOAuthProvider>
     </React.StrictMode>
 );
