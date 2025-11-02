2500b17b4a538cb54462187509b3a5116c6975cf
OleksandrGrinov <alex170707228@gmail.com>
2025-11-03 01:47:08 +0200

    refactor(app): simplify App routes and add dynamic imports

--- src/App.jsx
+++ src/App.jsx
@@ -17,65 +17,73 @@ import MyCertificates from "./pages/MyCertificates";
 import AdminEditTestPage from "./pages/AdminEditTestPage";
 import TestDetailsPage from "./pages/TestDetailsPage"; // new
 import CheckoutPage from "./pages/CheckoutPage";
-
+import AdminCreateTestPage from "./pages/AdminCreateTestPage.jsx";
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<HomePage />} />
-                    <Route path="tests" element={<TestsPage />} />
-                    <Route path="tests/:id" element={<TestPage />} />
-                    <Route path="tests/:id/details" element={<TestDetailsPage />} /> {/* add this */}
-                    <Route path="profile" element={<ProfilePage />} />
-                    <Route path="/verify/:cert_id" element={<VerifyCertificate />} />
-                    <Route path="achievements" element={<AchievementsPage />} />
-                    <Route
-                        path="analytics"
-                        element={
-                            <ProtectedRoute>
-                                <AnalyticsPage />
-                            </ProtectedRoute>
-                        }
-                    />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="tests" element={<TestsPage />} />
+          <Route path="tests/:id" element={<TestPage />} />
+          <Route path="tests/:id/details" element={<TestDetailsPage />} /> {/* add this */}
+          <Route path="profile" element={<ProfilePage />} />
+          <Route path="/verify/:cert_id" element={<VerifyCertificate />} />
+          <Route path="achievements" element={<AchievementsPage />} />
+          <Route
+            path="analytics"
+            element={
+              <ProtectedRoute>
+                <AnalyticsPage />
+              </ProtectedRoute>
+            }
+          />
 
-                    <Route path="verify/:token" element={<EmailVerifyPage />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/reset-password/:token" element={<ResetPassword />} />
-                    <Route path="my-certificates" element={<MyCertificates />} />
-                    <Route path="/checkout/:id" element={<CheckoutPage />} />
+          <Route path="verify/:token" element={<EmailVerifyPage />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="my-certificates" element={<MyCertificates />} />
+          <Route path="/checkout/:id" element={<CheckoutPage />} />
 
 
-                    {/* 🧱 Адмін маршрути — тільки через ProtectedRoute */}
-                    <Route
-                        path="admin"
-                        element={
-                            <ProtectedRoute requiredRole="admin">
-                                <AdminPage />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        path="admin/tests"
-                        element={
-                            <ProtectedRoute requiredRole="admin">
-                                <AdminTestsPage />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Route>
-                <Route
-                    path="admin/tests/:id/edit"
-                    element={
-                        <ProtectedRoute requiredRole="admin">
-                            <AdminEditTestPage />
-                        </ProtectedRoute>
-                    }
-                />
+          {/* 🧱 Адмін маршрути — тільки через ProtectedRoute */}
+          <Route
+            path="admin"
+            element={
+              <ProtectedRoute requiredRole="admin">
+                <AdminPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="admin/tests"
+            element={
+              <ProtectedRoute requiredRole="admin">
+                <AdminTestsPage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+        <Route
+          path="admin/tests/:id/edit"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminEditTestPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="admin/tests/create"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminCreateTestPage />
+            </ProtectedRoute>
+          }
+        />
 
-            </Routes>
-        </BrowserRouter>
-    );
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
