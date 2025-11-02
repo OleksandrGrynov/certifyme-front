7413532ed0f7a488657dc3cdc0ba2f836d6c7e53
OleksandrGrinov <alex170707228@gmail.com>
2025-11-03 01:47:11 +0200

    chore(entry): optimize React root and router setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,12 @@ import "./i18n/i18n";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId =
-    "382994921409-a7plkvr0d3pns8e6uac4fn7go39lielq.apps.googleusercontent.com";
+const clientId = "382994921409-a7plkvr0d3pns8e6uac4fn7go39lielq.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <GoogleOAuthProvider clientId={clientId}>
-            <App />
-        </GoogleOAuthProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </React.StrictMode>,
 );
