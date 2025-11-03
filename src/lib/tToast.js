1c9b38e1a857dae2a9f1fce3cfae8c95768993d6
OleksandrGrinov <alex170707228@gmail.com>
2025-11-03 23:06:33 +0200

    refactor: unify toast notifications system for global use

---
+++ src/lib/tToast.js
@@ -0,0 +1,15 @@
+// filepath: d:\coursehub-front\src\lib\tToast.js
+import toast from "react-hot-toast";
+import i18n from "../i18n/i18n";
+
+export const tLabel = (ua, en) => (i18n.language === "ua" ? ua : en);
+
+export const tToast = {
+  success: (ua, en, options) => toast.success(tLabel(ua, en), options),
+  error: (ua, en, options) => toast.error(tLabel(ua, en), options),
+  loading: (ua, en, options) => toast.loading(tLabel(ua, en), options),
+  message: (ua, en, options) => toast(tLabel(ua, en), options),
+};
+
+export default tToast;
+
