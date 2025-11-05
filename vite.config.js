35cec81dc52bf804bec40594e88909b4c86464e2
OleksandrGrinov <alex170707228@gmail.com>
2025-11-06 00:22:29 +0200

    fix vite outDir and vercel output directory

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "",
+  base: "", // залишаємо порожнім
   plugins: [react()],
+  build: {
+    outDir: "dist", // ⚡ обов'язково
+  },
 });
